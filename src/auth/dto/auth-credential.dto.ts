09ab2f899e83fa9476cfe02d5e63a79af2cc72a1
unknown <lunatics384@gmail.com>
2021-12-20 09:46:24 +0900

    [1220] Auth 유저 회원가입 유니크 필드 지정 및 예외처리 로직 추가

--- src/auth/dto/auth-credential.dto.ts
+++ src/auth/dto/auth-credential.dto.ts
@@ -1,4 +1,14 @@
+import {IsEmail, IsString, Matches, MaxLength, MinLength} from "class-validator";
+const MATCHES_ERROR_MESSAGE = "password only accepts english and number";
 export class AuthCredentialDto {
+    @IsEmail()
     email: string;
+
+    @IsString()
+    @MinLength(8)
+    @MaxLength(20)
+    @Matches(/^[a-zA-Z0-9]*$/, {
+        message: MATCHES_ERROR_MESSAGE,
+    })
     password: string;
 }
--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,12 +1,22 @@
 import {EntityRepository, Repository} from "typeorm";
 import {User} from "./user.entity";
 import {AuthCredentialDto} from "./dto/auth-credential.dto";
+import {ConflictException, InternalServerErrorException} from "@nestjs/common";
 
 @EntityRepository(User)
 export class UserRepository extends Repository<User>{
     async createUser(authCredentialDto: AuthCredentialDto):Promise<any>{
         const {email, password} = authCredentialDto;
         const user = this.create({email, password});
-        await this.save(user);
+        try{
+            await this.save(user);
+        }catch(e){
+            console.log(e);
+            if(e.code === "23505"){
+                throw new ConflictException("This email is already existing.");
+            }else{
+                throw new InternalServerErrorException();
+            }
+        }
     }
 }
